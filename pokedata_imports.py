07deff54c5e7ceb12858101f1b23778bfd91572d
Aviyan Acharya <avyn.xyz@gmail.com>
2024-05-04 19:24:25 +0545

    Added POKEMONJSON class to store misc Pokemon data such as type colors

--- pokedata_imports.py
+++ pokedata_imports.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 
 class Plot:
-    def __init__(self):
-        None
-    
+    def __init__(self) -> None:
+        pass
+
     def pie_chart(
             self,
             data,
@@ -16,3 +16,31 @@ class Plot:
         plt.pie(data, labels=labels, autopct=autopct)
         plt.title(title)
         plt.show()
+
+class PokemonJSON:
+    @staticmethod
+    def type_colors(poke_type:str):
+        color_dict = {
+            "Normal": "#A8A77A",
+            "Fire": "#EE8130",
+            "Water": "#6390F0",
+            "Electric": "#F7D02C",
+            "Grass": "#7AC74C",
+            "Ice": "#96D9D6",
+            "Fighting": "#C22E28",
+            "Poison": "#A33EA1",
+            "Ground": "#E2BF65",
+            "Flying": "#A98FF3",
+            "Psychic": "#F95587",
+            "Bug": "#A6B91A",
+            "Rock": "#B6A136",
+            "Ghost": "#735797",
+            "Dragon": "#6F35FC",
+            "Dark": "#705746",
+            "Steel": "#B7B7CE",
+            "Fairy": "#D685AD"
+        }
+        return color_dict[poke_type]
+    
+type_normal = Plot.type_colors("Normal")
+print(type_normal)
